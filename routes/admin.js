7859369c6d3125c425866b8edfc26f47ff934896
markandyuvasoft <markandyuvasoft408@gmail.com>
2022-10-13 12:05:53 -0700

    admin

---
+++ routes/admin.js
@@ -0,0 +1,69 @@
+import express from "express";
+import User from "../models/user.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import checkauth from "../middleware/auth.js";
+import adminauth from "../middleware/admin.js";
+import randomstring from 'randomstring'
+import nodemailer from 'nodemailer'
+
+
+const adminrouter = express.Router()
+
+//BCRYPT PASSWORD USE THIS METHOD START
+const secure2 = async (password) => {
+
+  try {
+    const passwordhash = await bcrypt.hash(password, 10)
+    return passwordhash
+
+  } catch (error) {
+    res.status(400).send({ message: "error" })
+  }
+}
+//BCRYPT PASSWORD USE THIS METHOD END
+
+
+//ADMIN LOGIN.....................................................................................
+adminrouter.post("/admin/login",async (req, res) => {
+
+    try {
+        
+        const email= req.body.email
+        const password= req.body.password
+
+        const userdata= await User.findOne({email:email})
+
+        if(userdata){
+
+            const passwordmatch= await bcrypt.compare(password,userdata.password)
+
+            if(passwordmatch){
+
+                if(userdata.isAdmin===false){
+
+                    res.status(400).send({message:"you are not admin"})
+                }else{
+                    res.status(200).send({message:"welcome admin..!!"})
+                }
+
+            }else{
+                res.status(400).send({message:"please try again"})
+            }
+
+        }else{
+            res.status(400).send({message:"please try again"})
+        }
+
+    } catch (error) {
+    
+        console.log("error");
+    }
+
+})
+
+
+
+
+export default adminrouter
+
