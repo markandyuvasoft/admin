65f050082eaa90a41fa2d35f6ef1a78035bedd26
markandyuvasoft <markandyuvasoft408@gmail.com>
2022-10-15 16:11:06 -0700

    isadisda

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,51 @@
 import jwt from 'jsonwebtoken'
 
 
+// const checkauth=(req,res,next)=>{
+
+//     const token = req.header('Authorization')
+
+//     if(!token){
+
+//         return res.status(401).send({error:'only authorised person'})
+//     }
+
+//     try{
+
+//         const decodeToken = jwt.verify(token,'privatekey')
+
+//         req.user = decodeToken
+
+//         next()
+
+//     }catch(e){
+
+//         return res.status(401).send({error:'wrong token'})
+//     }
+
+// }
+
+
 const checkauth=(req,res,next)=>{
-
-    const token = req.header('Authorization')
-
-    if(!token){
-
-        return res.status(401).send({error:'only authorised person'})
-    }
-
+    
     try{
+const token= req.headers.authorization.split(" ")[1]
+//  console.log(token)
+const verify = Jwt.verify(token,'i am markand')
 
-        const decodeToken = jwt.verify(token,'privatekey')
 
-        req.user = decodeToken
+next()
 
-        next()
-
-    }catch(e){
-
-        return res.status(401).send({error:'wrong token'})
     }
+    catch(error)
+    {
+        return res.status(401).json({
+            msg: 'only access authorised person'
+        })
+    }
+} 
 
-}
+
+
 
 export default checkauth;
