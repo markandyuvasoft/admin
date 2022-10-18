927b307b820f1ceae5033e811a68484722930ea5
markandyuvasoft <markandyuvasoft408@gmail.com>
2022-10-18 10:26:10 -0700

    admin jwt to txt

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,23 +1,3 @@
-// import mongoose from 'mongoose'
-// import User from '../models/user.js'
-
-// const adminauth=(req,res,next)=>{
-
-//     //const isAdmin = req.user.data;
-    
-//      const isAdmin = req.user.isAdmin;
-//     console.log(isAdmin);
-//     if(!req.user.isAdmin){
-
-//         return res.status(403).send({error:'you are not admin user'})
-//     }
-//     else{
-//         next()
-//     }
-// }
-// export default adminauth;
-
-
 
 import mongoose from 'mongoose'
 import User from '../models/user.js'
