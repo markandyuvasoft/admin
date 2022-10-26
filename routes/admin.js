91ddd98da1c6bfa35e49227346df6cffd4ab206f
markandyuvasoft <markandyuvasoft408@gmail.com>
2022-10-26 11:45:48 -0700

    block unblock spe

--- routes/admin.js
+++ routes/admin.js
@@ -69,45 +69,58 @@ adminrouter.post("/admin/login",async (req, res) => {
         }
 })
 
-
-
+//BLOCK THE USER START.............
 adminrouter.put("/block/:id",[checkauth,adminauth],async(req,res)=>{
-  try {
-    
-    const _id= req.params.id
-    // console.log(_id);
-    const isVarified= req.body.isVarified
 
-    const getid= await User.findByIdAndUpdate(_id,req.body.isVarified,{
-      new:true
-    })
-    // console.log(getid);
+  const _id= req.params.id
+  const isVarified= req.body.isVarified
 
-    const data= {
-      isVarified:0
-    }
+  const getid= await User.findByIdAndUpdate(_id,req.body.isVarified,{
+    new:true
+  })
+  const data= {
+    isVarified:1
+  }
+
 if(getid.isVarified==1){
-  //  user.isVarified=1;
 
+const data= {
+  isVarified:0
+}
+ const get= await User.findByIdAndUpdate(getid._id,data)
+ res.status(400).send({message:"block the user"})
+
+} else{
+res.status(400).send({message:"try again"})
+}
+})
+
+
+//UNBLOCK THE USER START.............
+adminrouter.put("/unblock/:id",[checkauth,adminauth],async(req,res)=>{
+ 
+  const _id= req.params.id
+  const isVarified= req.body.isVarified
+
+  const getid= await User.findByIdAndUpdate(_id,req.body.isVarified,{
+    new:true
+  })
   const data= {
     isVarified:0
   }
-   const get= await User.findByIdAndUpdate(getid._id,data)
-   res.status(200).send({success:"😭 block the user"})
-  //  console.log(get);
-} else{
-  const da1ta= {
-    isVarified:1
-  }
-  const getid1= await User.findByIdAndUpdate(getid._id,da1ta)
-  // console.log(getid1);
-  res.status(200).send({success:"🤗 unblock the user"})
-}
-  } catch (error) {
-    res.status(400).send("error")
-  }
-})
+if(getid.isVarified==0){
 
+    const da1ta= {
+      isVarified:1
+    }
+    const getid1= await User.findByIdAndUpdate(getid._id,da1ta)
+
+    res.status(400).send({message:"unblock the user"})
+  }
+  else{
+res.status(400).send({message:"try again"})
+}
+})
 
 
 //SEARCH USER START.................
