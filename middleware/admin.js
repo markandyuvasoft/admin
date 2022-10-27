b43ee43dcb2f0c989987ab181d48a9cd2a524976
markandyuvasoft <markandyuvasoft408@gmail.com>
2022-10-27 14:58:38 -0700

    chg

--- middleware/admin.js
+++ middleware/admin.js
@@ -3,13 +3,36 @@ import mongoose from 'mongoose'
 import User from '../models/user.js'
 import  Jwt  from 'jsonwebtoken'
 
-const adminauth=(req,res,next)=>{
+// const adminauth=(req,res,next)=>{
 
-        const token= req.headers.authorization.split(" ")[1]
-        const verify = Jwt.verify(token,'privatekey')
+//         const token= req.headers.authorization.split(" ")[1]
+//         const verify = Jwt.verify(token,'privatekey')
       
+//         const isAdmin = req.user.isAdmin;
+//         if(!isAdmin)
+//         {
+//             return res.status(403).send({error:'you are not admin user'})
+//         }else{
+
+//             next()
+//         }
+        
+// }
+
+
+
+const adminauth=(req,res,next)=>{
+    
+    const {authorization} = req.headers
+
+    if(!authorization){
+       return res.status(401).json({error:"only auth"})
+    }
+    const token = authorization.replace("Bearer ","")
+    Jwt.verify(token,"privatekey",(err,payload)=>{
         const isAdmin = req.user.isAdmin;
-        if(!isAdmin)
+
+       if(!isAdmin)
         {
             return res.status(403).send({error:'you are not admin user'})
         }else{
@@ -17,6 +40,11 @@ const adminauth=(req,res,next)=>{
             next()
         }
         
-}
+        
+    })
+
+
+} 
+
 
 export default adminauth;
